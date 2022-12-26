c427ad53ec10fb683efce9bf00b6c5a1d32f89c9
Lauge Hermansen <76589717+LaugeHermansen@users.noreply.github.com>
2022-12-26 20:25:53 +0100

    kjnkæjn

--- git_sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while true; do
-  git add .
-  git commit -m "Automatic commit"
-  git pull
-  git push
-  sleep 120
-done
--- test_grad_norm.py
+++ test_grad_norm.py
@@ -6,17 +6,16 @@ import matplotlib.pyplot as plt
 import os
 
 
-data = pd.read_csv("experiments/from_bottom_v2_42/log/version_1/metrics.csv")
+data = pd.read_csv("experiments/from_bottom_v2_42/log/version_2/metrics.csv")
 
-grad_norms = data["grad_norm_step"].dropna()
-val_losses = data["train_loss_step"].dropna()
+grad_norm_step = data["grad_norm_step"].dropna().values
+train_loss_step = data["train_loss_step"].dropna().values
 
-
-plt.plot(grad_norms)
+fig, axs = plt.subplots(1,2)
+axs[0].plot(grad_norm_step)
+axs[1].plot(train_loss_step)
+axs[1].plot(np.convolve(train_loss_step, np.ones(100)/100, mode='valid'))
 plt.show()
-plt.plot(val_losses)
-plt.show()
-
 
 
 
