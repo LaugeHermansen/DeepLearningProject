36dc2944bb0ffc6f551b901090b1d04a925e2c4c
LaugeHermansen <lauge.hermansen@hotmail.com>
2022-12-26 17:17:18 +0100

    running first from bottom

--- update_progress.sh
+++ update_progress.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # Set the path to the source file and the target file
-source_file="/path/to/source/file"
-target_file="/path/to/target/file"
+source_file="experiment.out"
+target_file="experiment.status.txt"
+
+# Check if the target file exists
+if [ ! -f "$target_file" ]
+then
+    # Create the target file if it doesn't exist
+    touch "$target_file"
+fi
 
 # Check if the source file has been modified
 while true
@@ -13,10 +20,10 @@ do
     # Check if the last line is different from the current contents of the target file
     if [ "$last_line" != "$(cat "$target_file")" ]
     then
-        # Update the target file with the last line of the source file
+        # Overwrite the contents of the target file with the last line of the source file
         echo "$last_line" > "$target_file"
     fi
 
     # Sleep for 1 second before checking for updates again
-    sleep 10
+    sleep 5
 done
