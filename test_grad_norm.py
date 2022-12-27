4016e625763358efc22021c839826cbd364e344a
Lauge Hermansen <76589717+LaugeHermansen@users.noreply.github.com>
2022-12-27 09:22:49 +0100

    none

--- test_grad_norm.py
+++ test_grad_norm.py
@@ -6,17 +6,26 @@ import matplotlib.pyplot as plt
 import os
 
 
-data = pd.read_csv("experiments/from_bottom_v2_42/log/version_2/metrics.csv")
+
+data = pd.read_csv("experiments/from_bottom_v3_42/log/version_0/metrics.csv")
 
 grad_norm_step = data["grad_norm_step"].dropna().values
 train_loss_step = data["train_loss_step"].dropna().values
-
+start = 100
 fig, axs = plt.subplots(2,2)
-axs[0,0].plot(grad_norm_step, alpha=0.2, markersize=2, marker='o', linestyle='None')
-axs[0,0].plot(np.convolve(grad_norm_step, np.ones(100)/100, mode='valid'))
-axs[0,1].plot(train_loss_step)
-axs[0,1].plot(np.convolve(train_loss_step, np.ones(100)/100, mode='valid'))
-axs[1,1].hist(grad_norm_step, bins=100)
+axs[0,0].plot(grad_norm_step[start:], alpha=0.2, markersize=2, marker='o', linestyle='None', label='grad_norm_step')
+axs[0,0].plot(np.convolve(grad_norm_step[start:], np.ones(100)/100, mode='valid'), label='grad_norm_step (smoothed)')
+axs[0,0].set_title('grad_norm_step')
+axs[0,0].set_ylim([0, 36])
+axs[0,0].legend()
+axs[0,1].plot(train_loss_step[start:], label='train_loss_step')
+axs[0,1].plot(np.convolve(train_loss_step[start:], np.ones(100)/100, mode='valid'), label='train_loss_step (smoothed)')
+axs[0,1].set_title('train_loss_step')
+axs[0,1].legend()
+axs[1,1].hist(grad_norm_step[start:], bins=100, label='histogram of grad_norm_step')
+axs[1,1].set_title('histogram of grad_norm_step')
+
+
 plt.show()
 
 
