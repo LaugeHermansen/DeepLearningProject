ae5e32000a679f357eb7a7fc261da8160bf939d3
LaugeHermansen <lauge.hermansen@hotmail.com>
2022-12-29 11:34:42 +0100

    none

--- teeeeeest.py
+++ teeeeeest.py
@@ -13,9 +13,13 @@ from tqdm import tqdm
 
 # # data = SpeechDatasetDisk(params)
 outs = []
-for i in tqdm(range(100)):
-    outs.append(glob("'/dtu/blackhole/11/155505/audio/**/*.wav'", recursive=True))
+for i in tqdm(range(1000)):
+    outs.append(glob("/dtu/blackhole/11/155505/audio/**/*.wav", recursive=True))
+
+assert len(outs[0]) != 0, "no files found"
 
 for k in tqdm(zip(*outs), total = len(outs[0])):
-    assert len(set(k)) == 1
-
+    assert len(set(k)) == 1, "not same order"
+    assert len(k) == len(outs)
+    
+print(f"Everything OK: The list of files were all in the same order: n_files={len(outs[0])}, n_trials={len(outs)}")
