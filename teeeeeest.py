ef12a6177e0bd0ce89f96caac2ab0ed22505bdf9
Lauge Hermansen <76589717+LaugeHermansen@users.noreply.github.com>
2022-12-29 15:13:42 +0100

    none

--- teeeeeest.py
+++ teeeeeest.py
@@ -5,8 +5,17 @@ import os
 
 audio_file_paths = np.load("val_files.npy")
 
+try:
+    for f in audio_file_paths:
+        # move file to validation folder
+        source = f.replace('/data/', '/audio/').replace('/dataset/','/')
+        target = source.replace('/train/', '/val/')
+        # os.renames(source, target)
+        assert os.path.exists(source)
+    
+    print("we're good")
+except:
+    print("we're NOT good")
 
-for f in audio_file_paths:
-    # move file to validation folder
-    target = f.replace('train', 'val')
-    os.renames(f, target)
+
+
