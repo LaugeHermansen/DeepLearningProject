f96ad7bfa5341a5e9e20ba9e9098ad9e75f08079
Lauge Hermansen <76589717+LaugeHermansen@users.noreply.github.com>
2022-12-29 15:23:52 +0100

    none

--- teeeeeest.py
+++ teeeeeest.py
@@ -2,10 +2,11 @@
 
 import numpy as np
 import os
+from tqdm import tqdm
 
 audio_file_paths = np.load("val_files.npy")
 
-for f in audio_file_paths:
+for f in tqdm(audio_file_paths):
     # move file to validation folder
     source = f.replace('/data/', '/audio/').replace('/dataset/','/')
     target = source.replace('/train/', '/val/')
