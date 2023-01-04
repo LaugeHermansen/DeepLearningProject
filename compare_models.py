59e3872e9bd0d5f3b64f85b10914c99885831828
LaugeHermansen <lauge.hermansen@hotmail.com>
2023-01-04 17:18:43 +0100

    none

--- compare_models.py
+++ compare_models.py
@@ -179,7 +179,7 @@ if __name__ == "__main__":
             model.spectrogram_upsampler = spec_ups
 
         # get the evaluator
-        evaluator = ModelEvaluator(model, exp_name, spec_dir, 200)
+        evaluator = ModelEvaluator(model, exp_name, spec_dir, 10)
         evaluator.evaluate(overwrite=False, parallel=False)
 
 
